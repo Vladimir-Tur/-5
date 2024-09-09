be8b53bb1a73e30afe7ea5fd081581cddfed2b76
Vladimir-Tur <vovanru2033@mail.ru>
2024-09-09 18:19:23 +0400

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,16 @@
+tuple_ = (1,2,3, [1,2])
+tuple_2 = True
+tuple_3 = 'chack'
+immutable_var = tuple_, tuple_2, tuple_3
+print(immutable_var)
+#immutable_var [3] = 5 3-ий символ-1 содержится в кортеже
+mutable_list = [1,2,3]
+print(mutable_list)
+mutable_list[0] = 2
+print(mutable_list)
+
+
+
+
+
+
