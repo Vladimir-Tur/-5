1dc1a4c372145d12e452b7a6c819883872ce62eb
Vladimir-Tur <vovanru2033@mail.ru>
2024-09-09 18:25:47 +0400

    Add files via upload

--- module_1_5.py
+++ module_1_5.py
@@ -1,4 +1,4 @@
-tuple_ = (1,2,3, [1,2])
+tuple_ = 1,2,3, [1,2]
 tuple_2 = True
 tuple_3 = 'chack'
 immutable_var = tuple_, tuple_2, tuple_3
@@ -8,6 +8,8 @@ mutable_list = [1,2,3]
 print(mutable_list)
 mutable_list[0] = 2
 print(mutable_list)
+print('Immutable var: ', immutable_var )
+print('Mutable list: ', mutable_list)
 
 
 
